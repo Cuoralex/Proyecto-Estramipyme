11d4f06a7ec491c2db853a761b416868444bf1e0
Cuoralex <alexacuartas@gmail.com>
2024-06-25 11:09:15 -0500

    Se generan componentes y se agrega bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./comunes/header/header.component";
+import { DashboardComponent } from "./paginas/dashboard/dashboard.component";
+import { FooterComponent } from "./comunes/footer/footer.component";
+import { LoginComponent } from './inicio/login/login.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, DashboardComponent, FooterComponent, LoginComponent]
 })
 export class AppComponent {
   title = 'Proyecto-Estramipyme';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './comunes/header/header.component';
+import { FooterComponent } from './comunes/footer/footer.component';
+import { DashboardComponent } from './paginas/dashboard/dashboard.component';
+import { LoginComponent } from './inicio/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    LoginComponent,
+    HttpClientModule,
+    
   ]
 })
 export class AppModule { }
