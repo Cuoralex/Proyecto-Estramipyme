9bcfc98a7768fea0d348f299def44335b8801efb
Cuoralex <alexacuartas@gmail.com>
2024-07-14 18:27:08 -0500

    generación de formulario

---
+++ src/app/components/views/form/form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './form/form.component.html',
+  styleUrl: './form/form.component.scss'
+})
+export class FormComponent {
+
+}
+
