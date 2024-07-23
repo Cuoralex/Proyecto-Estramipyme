92249927bfb4c79488a0e725f89b555696f18470
Cuoralex <alexacuartas@gmail.com>
2024-07-23 07:40:54 -0500

    Ajuste efecto banner

--- src/app/views/landing-page/landing-page.component.ts
+++ src/app/views/landing-page/landing-page.component.ts
@@ -1,7 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoginComponent } from '../../components/login/login.component';
 import { RegisterComponent } from '../../components/register/register.component';
-import { AppComponent } from "../../app.component";
 import { NavbarLandingpageComponent } from "../../shared/navbar-landingpage/navbar-landingpage.component";
 
 @Component({
@@ -10,13 +9,35 @@ import { NavbarLandingpageComponent } from "../../shared/navbar-landingpage/navb
   imports: [
     LoginComponent,
     RegisterComponent,
-    AppComponent,
     NavbarLandingpageComponent,
-    ],
+  ],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
-  providers:[],
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  private bannerItems!: NodeListOf<HTMLElement>;
+  private currentIndex = 0;
 
+  ngOnInit(): void {
+    const bannerContainer = document.getElementById('bannerContainer');
+    if (bannerContainer) {
+      this.bannerItems = bannerContainer.querySelectorAll('.banner-item') as NodeListOf<HTMLElement>;
+      
+      // Initialize: Hide all items except the first one
+      for (let i = 1; i < this.bannerItems.length; i++) {
+        this.bannerItems[i].style.opacity = '0';
+      }
+
+      // Set interval to change banner items
+      setInterval(() => this.showNextBannerItem(), 3000); // Change every 3 seconds
+    }
+  }
+
+  private showNextBannerItem(): void {
+    if (this.bannerItems.length > 0) {
+      this.bannerItems[this.currentIndex].style.opacity = '0';
+      this.currentIndex = (this.currentIndex + 1) % this.bannerItems.length;
+      this.bannerItems[this.currentIndex].style.opacity = '1';
+    }
+  }
 }
